5289176c75582f236367f06a357f9d8d21fafc95
minikin <djminikin@gmail.com>
2017-04-07 00:31:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+###Getting Started###
+
+Checkout this repo, install dependencies and start:
+
+```
+	> git clone git@github.com:minikin/reduxStarter.git
+	> cd reduxStarter
+	> npm install
+	> npm start
+```
---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    'webpack-hot-middleware/',
+    './src/index'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+    // js
+    {
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      include: path.join(__dirname, 'src')
+    },
+    // CSS
+    {
+      test: /\.styl$/,
+      include: path.join(__dirname, 'src'),
+      loader: 'style-loader!css-loader!stylus-loader'
+    }
+    ]
+  }
+};
